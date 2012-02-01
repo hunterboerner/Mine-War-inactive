d7db4957a08d8234401367d8874bef3864d2ea4a
Jozef Jaskolka <j2p1@hotmail.co.uk>
2012-02-01 22:09:51 +0000

    Now logs commands and changed "No arguments provided"

--- War/src/me/hunterboerner/war/Commands.java
+++ War/src/me/hunterboerner/war/Commands.java
@@ -1,5 +1,7 @@
 package me.hunterboerner.war;
 
+import java.util.logging.Logger;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,6 +19,7 @@ public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) { 
 		if(cmd.getName().equalsIgnoreCase("war")){ //if the player typed /war, do the following
+			Logger log = Logger.getLogger("Minecraft");
 			Player player = (Player) sender;
 				if (!(sender instanceof Player)) {
 					sender.sendMessage("This Command can only be run by a player!");
@@ -30,14 +33,15 @@ public class Commands implements CommandExecutor {
 				//return true;
 				
 				if (args.length == 0 ) {
-				player.sendMessage(ChatColor.RED + "No arguments provided.");
+				player.sendMessage(ChatColor.RED + "Need to add declare after /war");
 				// if you haven't put anything in, then it tells you that
 				return true;
 				}
 				else if (args[0].equals("declare")) {
 					if (args.length == 2){
 						player.sendMessage(ChatColor.RED + "You have declared war on " + args[1]);
-						player.getServer().broadcastMessage(ChatColor.RED + player.getName() + " has declared war on " + args[1]);
+						player.getServer().broadcastMessage(ChatColor.RED + player.getDisplayName() + " has declared war on " + args[1]);
+						log.info("[War] " + player.getDisplayName() + " used "  + "/war " + args[0] + " on " + args[1]);
 					return true;
 					}
 					else {
@@ -49,10 +53,8 @@ public class Commands implements CommandExecutor {
 				}
 				
 		// if this happened the function will break and return true. Otherwise it will go to false.
-			
-		
-		
-	}
+		}
 		return false;
 	}
 }
+
