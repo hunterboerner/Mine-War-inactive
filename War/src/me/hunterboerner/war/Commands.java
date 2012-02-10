055f316a9cd5bb6bcfe451acfdf734216270f3e5
Theron Boerner <hunterboerner@gmail.com>
2012-02-10 11:29:56 -0600

    Added Truce!

--- War/src/me/hunterboerner/war/Commands.java
+++ War/src/me/hunterboerner/war/Commands.java
@@ -18,7 +18,7 @@ public class Commands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) { 
-		if(cmd.getName().equalsIgnoreCase("war")) { //ADD PERMISSIONS HERE BEFORE this is just a test { if (player.hasPermission("ENTER.PERMISSIONS.HERE"))
+		if(cmd.getName().equalsIgnoreCase("war")) {{ if (player.hasPermission("war.declare"))
 			Logger log = Logger.getLogger("Minecraft");
 			Player player = (Player) sender;
 				if (!(sender instanceof Player)) {)
@@ -26,11 +26,6 @@ public class Commands implements CommandExecutor {
 					
 				return true;
 				}
-				
-				
-				 
-				//player.sendMessage(ChatColor.RED + "You have made " + "" + " An Ememy of you!");
-				//return true;
 				
 				if (args.length == 0 ) {
 				player.sendMessage(ChatColor.RED + "Need to add declare after /war");
@@ -60,6 +55,23 @@ public class Commands implements CommandExecutor {
 					log.info("[War] " + player.getDisplayName() + " used " + "/war " + args[0]);
 					return true;
 				}
+				else if (args[0].equals("truce")) {
+					//TODO Add permissions
+					if (args.length == 2){
+						player.sendMessage(ChatColor.GREEN + "You have made peace with " + args[1]);
+						player.getServer().broadcastMessage(ChatColor.GREEN + player.getDisplayName() + " has made peace with " + args[1]);
+						log.info("[War] " + player.getDisplayName() + " used "  + "/war " + args[0] + " on " + args[1]);
+				return true;
+					}
+					else {
+						player.sendMessage("Error, incorrect!");
+					}
+					//end of declare
+					
+					
+				return true;
+				}
+			
 				//end of world
 				
 		// if this happened the function will break and return true. Otherwise it will go to false.
