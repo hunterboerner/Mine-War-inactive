b514be769c815c247dfaa8dcddaa2a13cd4dd78f
Jozef Jaskolka <jozeth@hotmail.co.uk>
2012-02-20 19:46:23 +0000

    Permission should be fixed now...

--- War/src/me/hunterboerner/war/Commands.java
+++ War/src/me/hunterboerner/war/Commands.java
@@ -18,7 +18,7 @@ public class Commands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) { 
-		if(cmd.getName().equalsIgnoreCase("war")) {{ if (player.hasPermission("war"))
+		if(cmd.getName().equalsIgnoreCase("war")) if (player.hasPermission("jcommands.back")) {
 			Logger log = Logger.getLogger("Minecraft");
 			Player player = (Player) sender;
 				if (!(sender instanceof Player)) {)
